2e6231d8110e50c381a9269f5cedeaa9c512ddd3
Momo <mlloydtucker@smith.edu>
2023-02-13 17:33:12 -0500

    progress

--- Conversation.java
+++ Conversation.java
@@ -5,47 +5,55 @@ import java.util.Scanner;
 class Conversation {
 
   public static void main(String[] args) {
-    // Prompt user to enter number of rounds
-    Scanner input = new Scanner(System.in);
-    System.out.println("How many rounds?");
-    int Rounds = input.nextInt();
+    try (// Prompt user to enter number of rounds
+    Scanner input = new Scanner(System.in)) {
+      System.out.println("How many rounds?");
+      int Rounds = input.nextInt();
 
-    // Print greeting
-    System.out.println("Hi there! What's on your mind?");
+      // Print greeting
+      System.out.println("Hi there! What's on your mind?");
 
 
 
-      // Initialize variables
-      String userInput;
-      String response;
-      String[] responses = {
-          "Interesting, tell me more.",
-          "That sounds like a challenging problem.",
-          "That sounds like fun!",
-          "That's cool!"
-      };
+        // Initialize variables
+        String userInput;
+        String[] responses = {
+            "Interesting, tell me more.",
+            "That sounds like a challenging problem.",
+            "That sounds like fun!",
+            "That's cool!"
+        };
 
-      // Start conversation
-      for (int i = 0; i <= Rounds; i++) {
-          // Get user input
-          userInput = nextLine();
+        // Start conversation
+        for (int count = 0; count <= Rounds; count++) {
+            // Get user input
+            userInput = input.nextLine();
 
-          // Generate bot response
-          if (userInput.contains("I") || userInput.contains("you")|| userInput.contains("you")) {
-              // Mirror user input
-              response = userInput.replace("I", "you").replace("you", "I");
-          } else {
-              // Select random response
-              Random rand = new Random();
-              int randIndex = rand.nextInt(responses.length);
-              response = responses[randIndex];
+            // Generate bot response
+          String[] words = userInput.split(" ");
+          String response = " ";
+          for (int i = 0; i < words.length; i++) {
+            if (words[i].equalsIgnoreCase("I")) {
+              response += "You";
+            } else if (words[i].equalsIgnoreCase("you")) {
+              response += "I";
+            } else if (words[i].equalsIgnoreCase("me")) {
+              response += "you";
+            } else if (words[i].equalsIgnoreCase("am")) {
+              response += "are";
+            } else {
+              response += words[i];
+            }
+          response += " ";
           }
+        
 
-          // Print bot response
-          System.out.println(response);
-      }
 
-      // Print goodbye and conversation transcript
+            // Print bot response
+            System.out.println(response);
+        }
+    }
+    // Print goodbye and conversation transcript
       System.out.println("Well, it was nice talking to you. Here is a transcript of our conversation:");
       //System.out.println(userInput);
       //System.out.println(response);
