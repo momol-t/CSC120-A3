68c3fedb2cc7519ffbcdb699b0df8bf905df6084
Momo <mlloydtucker@smith.edu>
2023-02-15 19:58:55 -0500

    added transcript

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,8 @@
 
-//import java.util.Random;
-//import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 class Conversation {
@@ -10,11 +12,14 @@ class Conversation {
     Scanner input = new Scanner(System.in)) {
       System.out.println("How many rounds?");
       int Rounds = input.nextInt();
-    //Random Rand = new Random();
+      Random Rand = new Random();
+
+      List<String> transcript = new ArrayList<String>();
 
       // Print greeting
       System.out.println("Hi there! What's on your mind?");
 
+	transcript.add("Hi there! What's on your mind?");
 
 
         // Initialize variables
@@ -30,46 +35,57 @@ class Conversation {
         for (int count = 0; count <= Rounds; count++) {
             // Get user input
             userInput = input.nextLine();
+            transcript.add(userInput);
 
             // Generate bot response
           String[] words = userInput.split(" ");
-          String response = " ";
+          String response = "";
+          Boolean personal = false;
           for (int i = 0; i < words.length; i++) {
             if (i!=0){
               response+= " ";
             }
             if (words[i].equalsIgnoreCase("I")) {
               response += "You";
+              personal = true;
             } else if (words[i].equalsIgnoreCase("you")) {
               response += "I";
+              personal = true;
             } else if (words[i].equalsIgnoreCase("me")) {
               response += "you";
+              personal = true;
             } else if (words[i].equalsIgnoreCase("am")) {
               response += "are";
+              personal = true;
             } else if (words[i].equalsIgnoreCase("are")) {
               response += "am";
+              personal = true;
             } else if (words[i].equalsIgnoreCase("my")) {
               response += "your";  
+              personal = true;
             } else if (words[i].equalsIgnoreCase("your")) {
               response += "my";
+              personal = true;
             } else {
               response += words[i];
             }
           }
-        if (response.equals (userInput)){
-          response= cannedArray[3];
-
-
-          
+        if (!personal) {
+        // or if (response == userInput) {
+          int index = Rand.nextInt();
+          response = cannedArray[index % cannedArray.length];
+          transcript.add(response);
         }
+        
             // Print bot response
             System.out.println(response);
         }
-    }
+      }
     // Print goodbye and conversation transcript
       System.out.println("Well, it was nice talking to you. Here is a transcript of our conversation:");
       //System.out.println(userInput);
       //System.out.println(response);
-    }
+    System.out.println(Arrays.toString(transcript));
+    
   }
-
+}
